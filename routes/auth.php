15f604227413dc3017162702292039c8571e4801
Green-fad <194453259+Green-fad@users.noreply.github.com>
2025-10-10 01:09:10 -0400

    fix: Tentative de correction du routage pour le déploiement

--- routes/auth.php
+++ routes/auth.php
@@ -11,21 +11,21 @@ use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/register', [RegisteredUserController::class, 'create'])
-    ->middleware(['guest', 'setLanguage', 'checkCustomDomain'])
+    ->middleware(['guest', 'setLanguage'])
     ->name('register');
 
 Route::post('/register', [RegisteredUserController::class, 'store'])
     ->middleware(['guest', 'setLanguage']);
 
 Route::get('/login', [AuthenticatedSessionController::class, 'create'])
-    ->middleware(['guest', 'setLanguage', 'checkCustomDomain'])
+    ->middleware(['guest', 'setLanguage'])
     ->name('login');
 
 Route::post('/login', [AuthenticatedSessionController::class, 'store'])
     ->middleware(['guest', 'setLanguage']);
 
 Route::get('/forgot-password', [PasswordResetLinkController::class, 'create'])
-    ->middleware(['guest', 'setLanguage', 'checkCustomDomain'])
+    ->middleware(['guest', 'setLanguage'])
     ->name('password.request');
 
 Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])
